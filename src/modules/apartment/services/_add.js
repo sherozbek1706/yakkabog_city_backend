b8cd17b5a3276d06de31316f535d07b94a2987a1
Baxtiyorov She'rozbek <baxtiyorovsherozbek1111@gmail.com>
2024-09-23 15:42:49 +0500

    calculates the square meters of the apartment.

--- src/modules/apartment/services/_add.js
+++ src/modules/apartment/services/_add.js
@@ -2,7 +2,9 @@ const { db } = require("../../../db");
 
 module.exports = async ({ body, user }) => {
   let { id } = user;
+  let { field, price_per_square_meter } = body;
+  let total_price = field * price_per_square_meter;
   return await db("apartment")
-    .insert({ ...body, admin_id: id })
+    .insert({ ...body, total_price, admin_id: id })
     .returning("*");
 };
