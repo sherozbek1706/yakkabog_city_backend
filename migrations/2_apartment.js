d896ac5be3e06e1ae44a1d638d24e77bd0f32050
Baxtiyorov She'rozbek <baxtiyorovsherozbek1111@gmail.com>
2024-09-23 13:39:30 +0500

    To add apartments, typed migration, then admin_id was opened, where the admin is known

---
+++ migrations/2_apartment.js
@@ -0,0 +1,33 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("apartment", (table) => {
+    table.increments("id").primary();
+    table.integer("block_number").notNullable();
+    table.integer("entrance").notNullable();
+    table.integer("floor").notNullable();
+    table.integer("apartment_number").notNullable();
+    table.float("field").notNullable();
+    table.integer("number_of_rooms").notNullable();
+    table.float("one_square").notNullable();
+    table.float("total_sum").notNullable();
+    table.integer("total_floor").notNullable();
+    table.boolean("is_sold").defaultTo(false).notNullable();
+    table
+      .integer("admin_id")
+      .references("id")
+      .inTable("admin")
+      .onDelete("SET NULL");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("apartment");
+};
