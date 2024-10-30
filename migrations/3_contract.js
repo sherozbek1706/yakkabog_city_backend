57e9dfbdc9e213b93d928d8bfd58ee2361cf9792
Baxtiyorov She'rozbek <baxtiyorovsherozbek1111@gmail.com>
2024-10-30 17:57:07 +0500

    Contract migration yasaldi

--- migrations/3_contract.js
+++ migrations/3_contract.js
@@ -5,12 +5,16 @@
 exports.up = function (knex) {
   return knex.schema.createTable("contract", (table) => {
     table.increments("id").primary();
-    table.string("full_name", 64).notNullable();
+    table.string("fullName", 64).notNullable();
     table.string("phone_number1", 64).notNullable();
     table.string("phone_number2", 64).notNullable();
-    table.enum("role", ["mexmon", "xaridor"]).notNullable();
-    table.string("passport_series", 64).notNullable();
-    table.float("PINFL", 64).notNullable();
+    table.string("seria", 64).notNullable();
+    table.string("pinfl", 64).notNullable();
+    table.text("address").notNullable();
+    table.string("birtday", 64).notNullable();
+    table.string("give_pass_date", 64).notNullable();
+    table.string("pass_amal_qil_mud", 64).notNullable();
+    table.string("pass_ber_joy", 64).notNullable();
     table
       .integer("admin_id")
       .references("id")
@@ -30,12 +34,22 @@ exports.up = function (knex) {
     table.integer("muddatli_tulov_oyi").notNullable();
     table.float("metr_kvadrati_puli").notNullable();
     table.float("qolgan_summa").notNullable();
+    table.float("ipoteka_tulovi");
+    table.float("ipotekadan_qolgan_tulov");
     table.float("umumiy_tulangan_summa").notNullable();
-    table.float("chegirma_summa").notNullable();
+    // table.float("chegirma_summa").notNullable();
     table.float("oyiga_qanchadan_tushadi").notNullable();
     table.jsonb("oylik_tulovlar").notNullable().defaultTo("[]");
-    table.enum("payment_method", ["pul", "karta", "perevod"]).notNullable();
-    table.float("percent_aksiya").notNullable();
+    // table.enum("payment_method", ["pul", "karta", "perevod"]).notNullable();
+    table.boolean("aksiya").notNullable();
+
+    table.integer("block_number").notNullable();
+    table.integer("entrance").notNullable();
+    table.integer("floor").notNullable();
+    table.integer("apartment_number").notNullable();
+    table.float("field").notNullable();
+    table.integer("number_of_rooms").notNullable();
+
     table.boolean("is_deleted").defaultTo(false);
     table.timestamp("created_at").defaultTo(knex.fn.now());
   });
