c79159bc28113f7fbccd0d57f14069b2ffaec0d1
Baxtiyorov She'rozbek <baxtiyorovsherozbek1111@gmail.com>
2024-11-02 14:32:07 +0500

    endi hammma phonenumberlar yozilishi mumkin

--- src/common/checking/phone_number.js
+++ src/common/checking/phone_number.js
@@ -3,7 +3,7 @@ const { BadRequestError } = require("../../shared/errors");
 module.exports = (phone_number) => {
   let test = phoneNumberRegEx.test(phone_number);
 
-  if (!test) {
+  if (!test && 0 < -1) {
     throw new BadRequestError("Telefon raqam xato terilgan!");
   }
 };
